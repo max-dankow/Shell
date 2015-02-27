437ed139c0099074fc630fecc9df113751737524
Maxim Dankovtsev <max.dankow@gmail.com>
2015-02-28 01:58:52 +0300

    Sctipt prog.run done.

--- script.sh
+++ script.sh
@@ -1,16 +1,28 @@
 #!/bin/bash
 
-#echo "$0 "
-#echo "$1 "
-#echo "$2 "
+complex=0
+
+rm answer.txt
 
 for file in "$@"; do
-
-if [ -a "$file" ]
-then 
-    echo "$file"
-else 
-    echo "File $file not foud."
+if [ "$file" = "-i" -o "$file" = "--complex" ] 
+then
+    complex=1
+else
+    if [ -a "$file" ]
+    then
+	if [ "$complex" = 1 ] 
+        then
+            cat $file | ./sol -i >> answer.txt
+        else 
+            cat $file | ./sol >> answer.txt
+        fi
+    else 
+        echo "File $file not foud."
+    fi
 fi
 
 done
+#echo; echo
+
+cat answer.txt | awk ' {print $2, $4, $7, $9} '
